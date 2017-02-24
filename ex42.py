100407cb00c22a5f8e7c1be908bd24c4b019d783
Dorian Kcira <dkcira@gmail.com>
2017-02-24 12:11:01 -0800

    finish ex42

--- ex42.py
+++ ex42.py
@@ -1,11 +1,55 @@
-# Is-a, Has-a, Objects, and Classes
 
-## Animal is-a object (yes, sort of confusing) look at the extra credit
+"""Is-a, Has-a, Objects, and Classes"""
+
+
+# Animal is-a object (yes, sort of confusing) look at the extra credit
 class Animal(object):
-	pass
+    pass
 
-## Dog is an Animal
-class Dog(Animal):
-	def __init__(self, name):
-		## Dog has a name
-		self.name = name
+
+class Dog(Animal):  # Dog is an Animal
+    def __init__(self, name):
+        # Dog has a name
+        self.name = name
+
+
+class Cat(Animal):
+    def __init__(self, name):
+        self.name = name
+
+
+class Person(object):
+    def __init__(self, name):
+        self.name = name
+        # Person  has-a pet of some kind
+        self.pet = None
+
+
+class Employee(Person):
+    def __init__(self, name, salary):
+        super(Employee, self).__init__(name)
+        self.salary = salary
+
+
+class Fish(object):
+    pass
+
+
+class Salmon(Fish):
+    pass
+
+
+class Halibut(Fish):
+    pass
+
+
+# use the classes
+rover = Dog("Rover")
+satan = Cat("Satan")
+mary = Person("Mary")
+mary.pet = satan
+frank = Employee("Frank", 120000)
+frank.pet = rover
+flipper = Fish()
+crouse = Salmon()
+harry = Halibut()
