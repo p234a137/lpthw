5d71d493763c90c52ce1561ac09a3d96b08e49be
Dorian Kcira <dkcira@gmail.com>
2017-03-01 11:52:32 -0800

    add gitignore and update

--- projects/gothonweb/bin/app.py
+++ projects/gothonweb/bin/app.py
@@ -1,20 +1,24 @@
 """Exercise 50. Simple project with lpthw.web."""
-
 import web
 
 urls = (
-    '/', 'Index'
+    '/', 'Index',
+    '/foo', 'Foo'
 )
-
 app = web.application(urls, globals())
-
 render = web.template.render('templates/')
 
 
 class Index:
     def GET(self):
         greeting = "Hello World!"
-        return render.index(greeting = greeting)
+        return render.index(greeting=greeting)
+
+
+class Foo:
+    def GET(self):
+        message = "foo2"
+        return render.foo(message)
 
 
 if __name__ == "__main__":
